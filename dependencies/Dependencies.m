af1e436a26475c2d0077b42b537573c48e86f1ba
Christopher Tysh <chris@feltpad.net>
2016-07-26 05:47:37 -0500

    Initial Commit

---
+++ dependencies/Dependencies.m
@@ -0,0 +1,19 @@
+//
+//  Dependencies.m
+//  Dependencies
+//
+//  Created by Christopher Tysh on 7/26/16.
+//  Copyright © 2016 Christopher Tysh. All rights reserved.
+//
+
+#import "Dependencies.h"
+
+@implementation Dependencies
+
+- (void)addDirect:(NSString*)master withArray:(NSArray*)array {
+    [_masterDictionary setObject:array forKey:master];
+}
+
+- (void)dependenciesFor:(NSString*)value;
+
+@end
---
+++ dependencies/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  dependencies
+//
+//  Created by Christopher Tysh on 7/26/16.
+//  Copyright © 2016 Christopher Tysh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
